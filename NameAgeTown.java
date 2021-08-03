6e05d0b726e27e992ea76d4369598c405aeaf121
longnghts <82634370+longnghts@users.noreply.github.com>
2021-08-02 21:49:04 -0400

    init commit

---
+++ NameAgeTown.java
@@ -0,0 +1,7 @@
+public class NameAgeTown {
+  public static void main(String[] args) {
+    System.out.println("My Name is Mitchell");
+    System.out.println("I am 23 years old");
+    System.out.println("My hometown is Durham, NC");
+  }
+}
